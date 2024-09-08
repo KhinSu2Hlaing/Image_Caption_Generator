65f097eac49fe9148bfe141aa48e2ba845fe75c0
KhinSu2Hlaing <161063195+KhinSu2Hlaing@users.noreply.github.com>
2024-09-08 07:16:39 +0630

    Update ui.py

--- ui.py
+++ ui.py
@@ -12,7 +12,7 @@ mobilenet_model = MobileNetV2(weights="imagenet")
 mobilenet_model = Model(inputs=mobilenet_model.inputs, outputs=mobilenet_model.layers[-2].output)
 
 # Load your trained model
-model = tf.keras.models.load_model('my_best_model50(1).h5')
+model = tf.keras.models.load_model('my_best_model50 (1).h5')
 
 # Load the tokenizer
 with open('tokenizer.pickle', 'rb') as tokenizer_file:
